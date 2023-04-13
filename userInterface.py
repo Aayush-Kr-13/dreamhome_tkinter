3a1b3e768534b4de6dec72226e822ce0ca8090be
Aayush <2301aayush@gmail.com>
2023-04-14 01:16:26 +0530

    interest page

--- userInterface.py
+++ userInterface.py
@@ -12,6 +12,10 @@ def open_code_2():
 def open_code_3():
     root.destroy()
     os.system("python dreamhome_tkinter\\loginpage.py")
+    
+def open_code_4():
+    root.destroy()
+    os.system("python dreamhome_tkinter\\checkinterest.py")
 
 root = tk.Tk()
 root.title("Interface")
@@ -19,19 +23,21 @@ root.geometry("350x350")
 frame = tk.Frame(root, width=350, height=350, bg="#2C3E50")
 frame.pack(fill=tk.BOTH, expand=True)
 
-large_font = ('Verdana', 30)
+large_font = ('Verdana', 20)
 large_font1 = ('Verdana', 12)
 
 button1 = tk.Button(frame, text="Log Out",fg="red", command=open_code_3)
 button1.place(x=10,y=10)
 
 admin_label = tk.Label(frame, text="Welcome User!",bg="#2C3E50",fg="Orange",font=large_font)
-admin_label.place(x=25,y=45)
+admin_label.place(x=50,y=45)
 
 button1 = tk.Button(frame, text="Register Property", command=open_code_1,font=large_font1)
-button1.place(x=90, y=140)
+button1.place(x=90, y=120)
 button2 = tk.Button(frame, text="Search For Property", command=open_code_2,font=large_font1)
-button2.place(x=85,y=260)
+button2.place(x=85,y=190)
+button3 = tk.Button(frame, text="Check For Interest", command=open_code_4,font=large_font1)
+button3.place(x=85,y=270)
 
 
 root.mainloop()
