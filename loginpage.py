ea7f10dc186b30bf1cd7ee5be9d6d26439322db3
Aayush <2301aayush@gmail.com>
2023-04-30 15:27:09 +0530

    ui changed

--- loginpage.py
+++ loginpage.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import os
 
+bg="#2C3E50"
+textColor = "white"
+btnColor = "#5bc0be"
+
 def open_code_1():
     root.destroy()
     os.system("python dreamhome_tkinter\\userlogin.py")
@@ -19,24 +23,43 @@ def open_code_4():
 
 root = tk.Tk()
 root.title("Main Page")
-root.geometry("400x400")
-frame = tk.Frame(root, width=350, height=500, bg="#2C3E50")
+root.geometry("500x500")
+frame = tk.Frame(root, width=350, height=500, bg=bg)
 frame.pack(fill=tk.BOTH, expand=True)
 
-large_font = ('Verdana', 25)
+large_font = ('Verdana', 40)
 large_font1 = ('Verdana', 12)
+large_font2 = ('Verdana', 15)
+large_font3 = ('Arial', 10,'italic')
 
-login_label = tk.Label(frame, text="Dreamhome",bg="#2C3E50",fg="Orange",font=large_font)
-login_label.place(x=100,y=35)
+motto_label = tk.Label(frame, text=" find your home away from home",bg=bg,fg=textColor,font=large_font3)
+motto_label.place(x=150,y=105)
 
-button1 = tk.Button(frame, text="User Login", command=open_code_1,font=large_font1)
-button1.place(x=50,y=155)
-button2 = tk.Button(frame, text="Staff Login", command=open_code_2,font=large_font1)
-button2.place(x=245,y=155)
-button3 = tk.Button(frame, text="Admin Login", command=open_code_3,font=large_font1)
-button3.place(x=40,y=275)
-button4 = tk.Button(frame, text="User Registeration", command=open_code_4,font=large_font1)
-button4.place(x=215,y=275)
+login_label = tk.Label(frame, text="DreamHome",bg=bg,fg="orange",font=large_font)
+login_label.place(x=70,y=35)
 
+user_label = tk.Label(frame, text="Already a user?",bg=bg,fg=textColor,font=large_font2)
+user_label.place(x=40,y=160)
+
+button1 = tk.Button(frame, text="User Login",bg=btnColor,fg="black", command=open_code_1,font=large_font1)
+button1.place(x=60,y=225)
+
+staff_label = tk.Label(frame, text="Are you staff?",bg=bg,fg=textColor,font=large_font2)
+staff_label.place(x=300,y=160)
+
+button2 = tk.Button(frame, text="Staff Login",bg=btnColor,fg="black", command=open_code_2,font=large_font1)
+button2.place(x=315,y=225)
+
+admin_label = tk.Label(frame, text="Are you admin?",bg=bg,fg=textColor,font=large_font2)
+admin_label.place(x=40,y=325)
+
+button3 = tk.Button(frame, text="Admin Login",bg=btnColor,fg="black", command=open_code_3,font=large_font1)
+button3.place(x=60,y=385)
+
+new_label = tk.Label(frame, text="Are you new user?",bg=bg,fg=textColor,font=large_font2)
+new_label.place(x=290,y=325)
+
+button4 = tk.Button(frame, text="User Registeration",bg=btnColor,fg="black", command=open_code_4,font=large_font1)
+button4.place(x=305,y=385)
 
 root.mainloop()
